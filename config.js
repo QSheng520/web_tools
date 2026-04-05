135ada25265d466f6f7da555cc2217007a40dd43
QSheng <305082800@qq.com>
2026-04-06 00:59:00 +0800

    1.51 pv统计， 区分github发布和国内发布

---
+++ config.js
@@ -0,0 +1,18 @@
+/**
+ * 全局配置文件
+ * 统一管理网站配置信息
+ */
+
+const SITE_CONFIG = {
+    // 51.la 统计配置 --- 发布到github
+    analytics: {
+        id: "3PX8Y5WsT8DlC0L2",
+        ck: "3PX8Y5WsT8DlC0L2"
+    }
+
+    // // 51.la 统计配置 --- 发布到国内
+    // analytics: {
+    //     id: "3PX159tXXF8PlFb9",
+    //     ck: "3PX159tXXF8PlFb9"
+    // }
+};
